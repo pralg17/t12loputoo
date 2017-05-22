81f2fdcd1c89df5705639178c478958ddb043b35
Kristjan Liiva <krisliiv@greeny.cs.tlu.ee>
2017-05-22 16:52:53 +0300

    Added Final Work

---
+++ webmanager.js
@@ -0,0 +1,108 @@
+//Extension continues ------> Retrieving and managing saved websites
+
+//Retrieving data from Firebase
+var links = firebase.database().ref('Favourites').on('value', GetData, DataError);
+
+function GetData(data){
+
+    //Removing table row regeneration
+    var table_rows = document.querySelectorAll('#table_row');
+    for(i = 0; i < table_rows.length; i++){
+      table_rows[i].remove();
+    }
+
+
+    var Urls = data.val();
+    if(data.val() === null){
+      console.log("Databse is empty");
+      return;
+    }
+    var keys = Object.keys(Urls);
+    if(keys === null){
+      return;
+    }
+
+    //Creating the table for URL-s and tags
+    var table = document.getElementById('site_table');
+
+    for(i = 0; i < keys.length; i++){
+      var k = keys[i];
+      var link = Urls[k].Url;
+      var tag = Urls[k].Tag;
+      var date = Urls[k].Added;
+
+      var t_row = document.createElement('tr');
+      t_row.setAttribute('id', 'table_row');
+      table.appendChild(t_row);
+
+      var td1 = document.createElement('td');
+      var td2 = document.createElement('td');
+      var td3 = document.createElement('td');
+      var td4 = document.createElement('td');
+      t_row.appendChild(td1);
+      t_row.appendChild(td2);
+      t_row.appendChild(td3);
+      t_row.appendChild(td4);
+
+      var td1_content = document.createElement('a');
+      td1_content.setAttribute('id', 'url_listing');
+      td1_content.setAttribute('href', link);
+      td1_content.innerHTML += link;
+      td1.appendChild(td1_content);
+
+      td2.setAttribute('id', "url_tag");
+      td2.innerHTML += tag;
+
+      td3.setAttribute('id', "added_date");
+      td3.innerHTML += date;
+
+      var td4_content1 = document.createElement('button');
+      td4_content1.setAttribute('id', "delete_button");
+      td4_content1.setAttribute('type', "button");
+      td4_content1.className = "btn btn-danger btn-sm";
+
+      var td4_content2 = document.createElement('span');
+      td4_content2.className = "glyphicon glyphicon-trash";
+
+      td4_content1.appendChild(td4_content2);
+      td4.appendChild(td4_content1);
+
+    }
+
+}
+
+//Getting the refrence for deletion
+var rootRef = firebase.database().ref('Favourites');
+rootRef.on('value', function(snapshot){
+
+  var data = snapshot.val();
+  var keys = Object.keys(data);
+
+  for(i = 0; i < keys.length; i++){
+    var del_buttons = document.querySelectorAll('#delete_button');
+    for(i = 0; i < del_buttons.length; i++){
+      del_buttons[i].addEventListener('click', deleteData.bind(null,  keys[i]), false);
+    }
+  }
+
+});
+
+function DataError(data){
+    console.log("An error occurred, while retrieving data");
+    console.log(err);
+}
+
+
+function deleteData(tr_key){
+
+  var rootRef = firebase.database().ref().child('Favourites').child(tr_key);
+  rootRef.once('value', function(snapshot){
+    console.log(snapshot.val(), "has been removed");
+    if(snapshot.val() === null){
+      console.log("Data does not exist");
+    } else {
+      rootRef.remove();
+    }
+  });
+
+}
---
+++ websaver.js
@@ -0,0 +1,106 @@
+//Extension start -----> Save favourite websites
+
+//Links array for duplication check
+var links_array = [];
+
+var siteURL = window.location.href;
+
+//Performing double check for duplicates
+//Otherwise people can add duplicate websites
+//Will throw log an error If you're using the extension for the first time
+window.addEventListener('DOMContentLoaded', dataCheck(siteURL));
+
+console.log("Extension loaded");
+
+window.addEventListener('copy', function(){
+
+    //Getting the URL and adding it to Firebase
+    var newURL = window.location.href;
+    var duplicate_check = dataCheck(newURL);
+    var date_added = getCurrentDateAndTime();
+    var new_tag = prompt("What tag do you wish to add for this website?");
+
+    if(duplicate_check === true || new_tag === null || new_tag === ""){
+
+      console.log("An error occurred while adding to favourites");
+      console.log("Either the website already exists or you didn't enter a tag for the website");
+      return;
+
+    } else {
+
+        firebase.database().ref('Favourites/').push({
+            Url: newURL,
+            Tag: new_tag,
+            Added: date_added
+        });
+
+        console.log(newURL, "Added to favourites");
+
+    }
+
+  });
+
+//Duplication check
+function dataCheck(data){
+
+  var found;
+	var rootRef = firebase.database().ref().child("Favourites");
+	rootRef.on('value', function(snapshot){
+
+		var Urls = snapshot.val();
+		var keys = Object.keys(Urls);
+
+		for(i = 0; i < keys.length; i++){
+			var k = keys[i];
+			var link = Urls[k].Url;
+			links_array.push(link);
+		}
+
+	});
+
+  for(i = 0; i < links_array.length; i++){
+    if(links_array[i] === data){
+      found = true;
+    } else {
+      found = false;
+    }
+  }
+
+  links_array = []; //Emptying the array
+
+  return found;
+
+}
+
+function getCurrentDateAndTime(){
+
+  var currentDate = new Date();
+
+  var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+  var month = currentDate.getMonth();
+  var year = currentDate.getFullYear();
+  var monthday = currentDate.getDate();
+
+  var hours = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+  var seconds = currentDate.getSeconds();
+
+  var dateString = monthday + '.' + months[month] + '.' + year;
+  var timeString = addZeroBefore(hours) + ':' + addZeroBefore(minutes) + ':' + addZeroBefore(seconds);
+
+  var full_dnt = dateString + " " + timeString;
+
+  return full_dnt;
+
+}
+
+function addZeroBefore(timeNumber) {
+    if (timeNumber < 10) {
+
+        timeNumber = '0' + timeNumber;
+
+    }
+
+    return timeNumber;
+}
