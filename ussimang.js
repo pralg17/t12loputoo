a1ce991308ae1626d5ba2bc9c58c36de23b66cb6
Stiv0 <stivo.sirel@gmail.com>
2017-05-23 12:06:10 +0300

    ALL DONE

--- ussimang.js
+++ ussimang.js
@@ -1,24 +1,38 @@
 var canvas;
 var ctx;
 var direction;
-var score = 0;
+var score = 1;
+var savedScore;
 var keyPressed = "";
 var refreshInterval;
+var dead = false;
+var bestScore;
 
 //snake
-var snakeRect = 20;
+var snakeRect = 40;
 var snakeSpeed = 200;
+var snakeSpeedMax = snakeSpeed;
+var snakeHistoryX = [];
+var snakeHistoryY = [];
 var snakeTailX = [];
 var snakeTailY = [];
 var snakeX = 240;
 var snakeY = 240;
+var snakeColor = "white";
 
 //food
-var foodRect = 20;
+var foodRect = 40;
 var foodX;
 var foodY;
+var foodColor = "darkred";
+
+//audio
+var eat = new Audio("eat.mp3");
+var theme = new Audio("theme.wav");
+var lose = new Audio("lose.mp3");
 
 window.onload = function(){
+	bestScore = localStorage.getItem('Score');
 	canvas = document.getElementById("paber");
 	ctx = canvas.getContext("2d");
 	document.getElementById("startBtn").addEventListener("click", startGame);
@@ -29,11 +43,19 @@ function startGame(){
 	document.getElementById("startBtn").removeEventListener("click", startGame);
 	console.log("Game started!")
 	window.addEventListener("keydown", whatKey);
+	newFood();
 	drawSnake();
-	drawFood();
+	document.getElementById("instructions").innerHTML = "Liikumiseks kasuta nooleklahve";
+	document.getElementById("startBtn").style = "";
+	document.getElementById("paber").style.backgroundImage = "url('bg.jpg')";
+	
+	theme.play();
 }
 
 function whatKey(e){
+	document.getElementById("instructions").innerHTML = "Best: "+bestScore;
+	document.getElementById("score").innerHTML = score-1;
+
 	setTimeout(function(){
 		if(e.keyCode == 38){
 			if(keyPressed == "down" || keyPressed == "up"){} 
@@ -71,55 +93,107 @@ function whatKey(e){
 }
 
 function drawSnake(){
-	//kontrollib kas uss on läinud üle canvase ääre
-	if(snakeX<(canvas.width-canvas.width)){
-		snakeX = canvas.width-snakeRect;
-	}
-	if(snakeX>canvas.width-snakeRect){
-		snakeX = 0;
-	}
-	if(snakeY<(canvas.height-canvas.height)){
-		snakeY = canvas.height-snakeRect;
-	}
-	if(snakeY>canvas.height-snakeRect){
-		snakeY = 0;
-	}
-	
-	//joonistab ussi
-	ctx.beginPath();
-		ctx.rect(snakeX, snakeY, snakeRect, snakeRect);
-		ctx.fill();
-	ctx.closePath();
-	//kontrollib, kas toit on söödud
-	eatFood();
-}
-
-function eatFood(){
-	if(foodX == snakeX && foodY == snakeY){
-		score++;
-		document.getElementById("score").innerHTML = score;
+	if(dead == false){
+		//kontrollib kas uss on läinud üle canvase ääre
+		if(snakeX<(canvas.width-canvas.width)){
+			snakeX = canvas.width-snakeRect;
+		}
+		if(snakeX>canvas.width-snakeRect){
+			snakeX = 0;
+		}
+		if(snakeY<(canvas.height-canvas.height)){
+			snakeY = canvas.height-snakeRect;
+		}
+		if(snakeY>canvas.height-snakeRect){
+			snakeY = 0;
+		}
+		
+		snakeHistoryX.push(snakeX);
+		snakeHistoryY.push(snakeY);
+		
+		snakeTailX.push(snakeX);
+		snakeTailY.push(snakeY);
+		
+		if(score == 1 || score == snakeTailX.length){
+			snakeTailX.shift();
+			snakeTailY.shift();
+			console.log(snakeTailX, snakeTailY);
+		}
+		
+		//kontrollib surma
+		checkDeath();
+		
+		//kustutab ussi viimase ruudu
+		ctx.clearRect(snakeHistoryX[snakeHistoryX.length-1-score], snakeHistoryY[snakeHistoryY.length-1-score], snakeRect, snakeRect);
+		
+		//joonistab ussi esimese ruudu
+		ctx.beginPath();
+			ctx.rect(snakeX, snakeY, snakeRect, snakeRect);
+			ctx.fillStyle = snakeColor;
+			ctx.fill();
+		ctx.closePath();
+		//kontrollib, kas toit on söödud
+		eatFood();
+		//joonistab toidu
 		drawFood();
 	}
 }
 
 function drawFood(){
+	ctx.beginPath();
+		ctx.rect(foodX, foodY, foodRect, foodRect);
+		ctx.fillStyle = foodColor;
+		ctx.fill();
+	ctx.closePath();
+}
+
+function eatFood(){
+	if(foodX == snakeX && foodY == snakeY){
+		eat.play();
+		score++;
+		snakeSpeed = snakeSpeedMax - score*2.5;
+		document.getElementById("score").innerHTML = score-1;
+		newFood();
+	}
+}
+
+function newFood(){
 	var canvasDivided = canvas.width/snakeRect-1;
 	foodX = Math.round(Math.random()*canvasDivided)*snakeRect;
 	foodY = Math.round(Math.random()*canvasDivided)*snakeRect;
-	ctx.beginPath();
-		ctx.rect(foodX, foodY, foodRect, foodRect);
-		ctx.fill();
-	ctx.closePath();
 	console.log(foodX, foodY, foodRect, foodRect);
+}
+
+function checkDeath(){
+		var j = 1;
+		for(var i = 0;i<snakeTailX.length;i++){
+			console.log(i, j);
+			for(j;j<snakeTailX.length;j++){
+				if(snakeTailX[i] == snakeTailX[j] && snakeTailY[i] == snakeTailY[j]){
+					dead = true;
+					console.log("surnud");
+					lose.play();
+					canvas.width = canvas.width;
+					document.getElementById("paber").style.backgroundImage = "url('snek.jpg')";
+					setTimeout(function(){
+						if(score>bestScore || bestScore == null){
+							localStorage.clear();
+							bestScore = score-1;
+							localStorage.setItem('Score', bestScore);
+						}
+						alert("!!Mäng läbi!!\nSinu skooriks sai "+ (score-1));
+						location.reload();}, 2500);
+				}
+			}
+		
+		}
 }
 
 function moveUp(){
 	if(keyPressed == "up"){
-		ctx.clearRect(snakeX, snakeY, snakeRect, snakeRect);
 		snakeY = snakeY - snakeRect;
 		drawSnake();
 		refreshInterval = setInterval(function(){
-			ctx.clearRect(snakeX, snakeY, snakeRect, snakeRect);
 			snakeY = snakeY - snakeRect;
 			drawSnake();}, snakeSpeed);
 	}
@@ -127,23 +201,19 @@ function moveUp(){
 
 function moveDown(){
 	if(keyPressed == "down"){
-		ctx.clearRect(snakeX, snakeY, snakeRect, snakeRect);
 		snakeY = snakeY + snakeRect;
 		drawSnake();
 		refreshInterval = setInterval(function(){
-			ctx.clearRect(snakeX, snakeY, snakeRect, snakeRect);
 			snakeY = snakeY + snakeRect;
 			drawSnake();}, snakeSpeed);
 	}
 }
 
 function moveLeft(){
-	ctx.clearRect(snakeX, snakeY, snakeRect, snakeRect);
 		snakeX = snakeX - snakeRect;
 		drawSnake();
 	if(keyPressed == "left"){
 		refreshInterval = setInterval(function(){
-			ctx.clearRect(snakeX, snakeY, snakeRect, snakeRect);
 			snakeX = snakeX - snakeRect;
 			drawSnake();}, snakeSpeed);
 	}
@@ -151,11 +221,9 @@ function moveLeft(){
 
 function moveRight(){
 	if(keyPressed == "right"){
-		ctx.clearRect(snakeX, snakeY, snakeRect, snakeRect);
 		snakeX = snakeX + snakeRect;
 		drawSnake();
 		refreshInterval = setInterval(function(){
-			ctx.clearRect(snakeX, snakeY, snakeRect, snakeRect);
 			snakeX = snakeX + snakeRect;
 			drawSnake();}, snakeSpeed);
 	}
