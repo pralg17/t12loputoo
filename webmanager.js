dba4a35f88c93fcab1bbac75f07e103e14ba348a
Kristjan Liiva <krisliiv@greeny.cs.tlu.ee>
2017-05-23 11:25:16 +0300

    Eksamil lisatud osa

--- webmanager.js
+++ webmanager.js
@@ -87,10 +87,16 @@ rootRef.on('value', function(snapshot){
 
 });
 
+
 function DataError(data){
     console.log("An error occurred, while retrieving data");
     console.log(err);
 }
+
+//Adding an eventlistener to the search button
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("search_button").addEventListener("click", searchDataByTag);
+});
 
 
 function deleteData(tr_key){
@@ -105,4 +111,66 @@ function deleteData(tr_key){
     }
   });
 
+}
+
+function searchDataByTag(){
+	
+	var value = document.getElementById('search_field').value;	
+	
+	var ref = firebase.database().ref('Favourites').orderByChild('Tag').equalTo(value);
+	ref.on('value', function(snapshot){
+		
+				
+		var table_rows = document.querySelectorAll('#table_row2');
+		for(i = 0; i < table_rows.length; i++){
+			table_rows[i].remove();
+		}
+		
+		
+		var data = snapshot.val();
+		
+		if(data === null){
+			console.log("No data was found");
+			return;
+		}
+		
+		var keys = Object.keys(data);
+		
+		var table = document.getElementById('new_table');
+		
+		for(i = 0; i < keys.length; i++){
+			var k = keys[i];
+			var link = data[k].Url;
+			var tag = data[k].Tag;
+			var date = data[k].Added;
+			
+			var t_row = document.createElement('tr');
+			t_row.setAttribute('id', 'table_row2');
+			table.appendChild(t_row);
+
+			var td1 = document.createElement('td');
+			var td2 = document.createElement('td');
+			var td3 = document.createElement('td');
+			var td4 = document.createElement('td');
+			t_row.appendChild(td1);
+			t_row.appendChild(td2);
+			t_row.appendChild(td3);
+			t_row.appendChild(td4);
+
+			var td1_content = document.createElement('a');
+			td1_content.setAttribute('id', 'url_listing2');
+			td1_content.setAttribute('href', link);
+			td1_content.innerHTML += link;
+			td1.appendChild(td1_content);
+
+			td2.setAttribute('id', "url_tag2");
+			td2.innerHTML += tag;
+
+			td3.setAttribute('id', "added_date2");
+			td3.innerHTML += date;
+			
+		}
+		
+	});
+	
 }
