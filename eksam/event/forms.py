aee10b1b1df36a2707bfcac2ce9a83512d99e8c2
jantreiberg <jantreiberg@users.noreply.github.com>
2017-05-29 15:03:49 +0300

    Eksam J.T
    Proovisin juurde lisada Geoposition'i, mis väga ei õnnestunud.
    Rakendus on kirjutatud Djangos

---
+++ eksam/event/forms.py
@@ -0,0 +1,12 @@
+""" Django forms """
+from django import forms
+
+from .models import Event
+
+
+class EventForm(forms.ModelForm):
+    """ Form to create new events """
+    class Meta:
+        """ Tell Django which model should be used to create this form  """
+        model = Event
+        fields = ('title', 'description', 'location', 'event_date')
---
+++ eksam/event/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from .import views
+
+urlpatterns = [
+    url(r'^$', views.event_list, name='event_list'),
+    url(r'^event/(?P<pk>\d+)/$', views.event_detail, name='event_detail'),
+    url(r'^event/new/$', views.event_new, name='event_new'),
+    url(r'^event/(?P<pk>\d+)/edit/$', views.event_edit, name='event_edit'),
+    url(r'^event/(?P<pk>\d+)/remove/$', views.event_remove, name='event_remove'),
+]
