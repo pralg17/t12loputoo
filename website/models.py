454b0a1a120ba48299c1862fe2081b9597b370c2
Marko <mart@praktika1.cs.tlu.ee>
2017-05-30 06:34:13 +0300

    Django algfailid

---
+++ website/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+from django.utils import timezone
+
+
+class Content(models.Model):
+	user = models.ForeignKey("auth.User")
+	Tekst = models.TextField(default="")
+	created = models.DateTimeField(default=timezone.now())
+
+	def __str__(self):
+		return self.user +  "_" + self.created
